b23f4b78b1424eee823943444106e7196c0d4ddf
Neto6391 <neto6391@gmail.com>
2020-04-02 00:58:30 -0300

    prepare home controller

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,9 @@
 import { Router } from 'express';
 
+import HomeController from '../app/controllers/HomeController';
+
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  res.json({ message: 'Hello World' });
-});
+routes.get('/', HomeController.index);
 
 export default routes;
